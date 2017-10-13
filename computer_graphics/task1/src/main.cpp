d9d99118fde6ca30781178aca45a0770fdb3f238
forest0 <forest9643@gmail.com>
2017-10-13 21:29:57 +0800

    move the right brace to the end of the line

--- computer_graphics/task1/src/main.cpp
+++ computer_graphics/task1/src/main.cpp
@@ -1,8 +1,7 @@
 #include "mainwindow.h"
 #include <QtWidgets/QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
--- computer_graphics/task1/src/mainwindow.cpp
+++ computer_graphics/task1/src/mainwindow.cpp
@@ -3,12 +3,11 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow) {
+
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
